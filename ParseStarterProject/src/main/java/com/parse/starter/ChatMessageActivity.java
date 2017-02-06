6814bae0ef53af5c809c955ef51163032c5ef755
OneS <lzdeng@gmail.com>
2017-02-07 00:51:34 +0800

    Initial version 0.0.2
    backup code 2017/2/7

--- ParseStarterProject/src/main/java/com/parse/starter/ChatMessageActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ChatMessageActivity.java
@@ -7,25 +7,90 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static com.parse.ParseQuery.getQuery;
 
 public class ChatMessageActivity extends AppCompatActivity {
     ParseObject parseObject = new ParseObject("message");
+    EditText chatEditText;
+    String opponentName = "";
+    String userName = "";
 
     ListView messageListView;
     ArrayList<String> messageArrayList = new ArrayList<String>();
     ArrayAdapter arrayAdapter;
 
+    public void onSendMessage(View view)
+    {
+         String message = chatEditText.getText().toString();
+         String recipient = opponentName;
+         String sender = userName;
+
+         parseObject.put("recipient", recipient);
+         parseObject.put("sender", sender);
+         parseObject.put("message", message);
+
+         parseObject.saveInBackground(new SaveCallback() {
+             @Override
+             public void done(ParseException e) {
+                 if(e != null)
+                 {
+                     Toast.makeText(ChatMessageActivity.this, "Error: "+e.toString(), Toast.LENGTH_SHORT).show();
+
+                 }
+                 else if(e == null)
+                 {
+                     onUpdateMessage();
+                 }
+             }
+         });
+
+    }
+
     public void onUpdateMessage()
     {
         messageArrayList.clear();
-        messageArrayList.add("test message");
+        //messageArrayList.add("test message");
+
+        ParseQuery<ParseObject> parseQuery = new ParseQuery<ParseObject>("message");
+
+        parseQuery.whereEqualTo("recipient", opponentName);
+        parseQuery.orderByAscending("createdAt");
+        parseQuery.setLimit(20);
+
+        parseQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e == null)
+                {
+                    for(ParseObject object : objects)
+                    {
+                        String recipient = object.get("recipient").toString();
+                        String sender = object.get("sender").toString();
+                        String message = object.get("message").toString();
+                        messageArrayList.add(sender+" -> "+recipient+" : "+message);
+                    }
+
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
+        });
 
         arrayAdapter.notifyDataSetChanged();
     }
@@ -34,12 +99,13 @@ public class ChatMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_message);
+        chatEditText = (EditText)findViewById(R.id.chatEditText);
         messageListView = (ListView)findViewById(R.id.chatContentListView);
         arrayAdapter = new ArrayAdapter(ChatMessageActivity.this,android.R.layout.simple_list_item_1,messageArrayList);
 
         Intent intent = getIntent();
-        String opponentName = intent.getStringExtra("opponentName");
-        String userName = ParseUser.getCurrentUser().getUsername();
+        opponentName = intent.getStringExtra("opponentName");
+        userName = ParseUser.getCurrentUser().getUsername();
         setTitle(opponentName+"     ( "+userName+" Login )");
 
         messageListView.setAdapter(arrayAdapter);
